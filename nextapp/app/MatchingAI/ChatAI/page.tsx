e748b96a5264f5f63eef253152bb04a15f2620ad
dnwls6102 <dnwls6102@gmail.com>
2024-12-30 00:27:17 +0900

    Squashed commit of the following:
    commit 5979ca8c169e3c5c747002766c1fb5de8704bfee
    Author: dnwls6102 <dnwls6102@gmail.com>
    Date:   Mon Dec 30 00:24:27 2024 +0900
    
        [feat] STT 다양한 방식을 테스트하는 페이지 추가
    
    commit 22304187e6832b47cbe69bb1114779d75ffd35bb
    Author: dnwls6102 <dnwls6102@gmail.com>
    Date:   Sun Dec 29 23:49:48 2024 +0900
    
        [feat] 실시간 음성 처리의 불완전 구현
    
    commit cb72c6cf7efc5bdc83bf1b1f97e957af09ccab4f
    Author: dnwls6102 <dnwls6102@gmail.com>
    Date:   Sun Dec 29 21:07:51 2024 +0900
    
        [feat]STT 구현 테스트를 위한 AudioTest 페이지 작성
        [feat]클라이언트에서 소리를 입력받아 API를 활용해 텍스트로 변환(버튼 눌러서)
    
    commit df0438e18ed4b5638e7d4ea76bfce67dc416fbed
    Author: dnwls6102 <dnwls6102@gmail.com>
    Date:   Sat Dec 28 19:57:00 2024 +0900
    
        Squashed commit of the following:
    
        commit 8c0a7581d453d7b9379175529c2b6143f7bbff6c
        Merge: 6b7594a 2b0a118
        Author: sookyungchae12 <sookungchae@gmail.com>
        Date:   Sat Dec 28 19:43:54 2024 +0900
    
            Merge branch 'skc' into dev
    
        commit 2b0a118a9239fed85749b5e54ff66e511eb1a085
        Author: sookyungchae12 <sookungchae@gmail.com>
        Date:   Sat Dec 28 19:14:08 2024 +0900
    
            [feat] Feedback 생성
    
        commit 6b7594a02b8d26b501af3ed506e49e377e26eecc
        Merge: 01aa8d7 b01ab65
        Author: minyeong-lee <lmy1366@gmail.com>
        Date:   Sat Dec 28 15:18:40 2024 +0900
    
            Merge branch 'dev' of https://github.com/dnwls6102/SoSweet into dev
    
        commit 01aa8d77d4de21d6ab5546ec3a446950f6c5c498
        Author: minyeong-lee <lmy1366@gmail.com>
        Date:   Sat Dec 28 15:04:20 2024 +0900
    
            [chore] LF 및 BackEnd 환경 설정
    
    commit dc0bd289e1829bb3bdd2125a404ec17fa49a4245
    Author: dnwls6102 <dnwls6102@gmail.com>
    Date:   Sat Dec 28 19:50:41 2024 +0900
    
        [fix] 컴파일 오류 수정
        [feat] Whisper 적용 작업중
    
    commit 166895cb09c3f5e86fbbc19f242b225eea2f5b06
    Author: dnwls6102 <dnwls6102@gmail.com>
    Date:   Sat Dec 28 15:40:52 2024 +0900
    
        [feat] openai 패키지 추가
        [fix] 컴파일 오류 수정중

--- nextapp/app/MatchingAI/ChatAI/page.tsx
+++ nextapp/app/MatchingAI/ChatAI/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
-import Videobox from '@/components/videobox';
+import Videobox from '../../../components/videobox'; // 상대 경로로 수정
 
 export default function Chat() {
+  const [isRecording, setIsRecording] = useState(false);
+  const [transcript, setTranscript] = useState('');
+  const [audioChunks, setAudioChunks] = useState<Blob[]>([]); // 오디오 청크 저장
+  const [vadTimeout, setVadTimeout] = useState<NodeJS.Timeout | null>(null); // VAD 타임아웃(녹음 종료)
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null); // 미디어 레코더 참조
   const videoref = useRef<HTMLVideoElement>(null);
   const router = useRouter();
   const keys = '호감도';
@@ -14,6 +19,84 @@ export default function Chat() {
   const handleNavigation = () => {
     router.push('/Feedback');
   };
+
+  // 녹음 시작하는 함수
+  const handleStartRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaRecorderRef.current = new MediaRecorder(stream);
+      mediaRecorderRef.current.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          setAudioChunks((prev) => [...prev, event.data]);
+        }
+      };
+      mediaRecorderRef.current.onstop = async () => {
+        const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+        const formData = new FormData();
+        formData.append('file', audioBlob, 'audio.wav');
+
+        try {
+          const response = await fetch(
+            'https://api.openai.com/v1/audio/transcriptions',
+            {
+              method: 'POST',
+              headers: {
+                Authorization: `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`,
+              },
+              body: formData,
+            },
+          );
+
+          if (response.ok) {
+            const result = await response.json();
+            setTranscript(result.transcript);
+          } else {
+            console.error('Error1:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error2:', error);
+        }
+      };
+
+      mediaRecorderRef.current.start();
+      setIsRecording(true);
+      detectSilence(mediaRecorderRef.current);
+    } catch (error) {
+      console.error('Error accessing microphone:', error);
+    }
+  };
+
+  // 녹음 종료하는 함수
+  const handleStopRecording = () => {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      setIsRecording(false);
+    }
+  };
+
+  // 침묵 감지하는 함수
+  const detectSilence = (mediaRecorder: MediaRecorder) => {
+    if (vadTimeout) clearTimeout(vadTimeout);
+    setVadTimeout(
+      setTimeout(() => {
+        console.log('Silence detected, stop recording');
+        mediaRecorder.stop();
+        handleStopRecording();
+      }, 3000),
+    );
+  };
+
+  useEffect(() => {
+    handleStartRecording();
+    return () => {
+      if (mediaRecorderRef.current) {
+        mediaRecorderRef.current.stream
+          .getTracks()
+          .forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.left}>
@@ -23,7 +106,7 @@ export default function Chat() {
         <textarea
           className={styles.textarea}
           readOnly
-          defaultValue={`침울한 표정 1분 지속 : 호감도 - 10%\n주제 파악을 못함 : 호감도 - 10%`}
+          value={transcript}
         ></textarea>
         <button className={styles.endButton} onClick={handleNavigation}>
           대화 종료
