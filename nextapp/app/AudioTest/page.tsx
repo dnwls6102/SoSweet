22304187e6832b47cbe69bb1114779d75ffd35bb
dnwls6102 <dnwls6102@gmail.com>
2024-12-29 23:49:48 +0900

    [feat] 실시간 음성 처리의 불완전 구현

--- nextapp/app/AudioTest/page.tsx
+++ nextapp/app/AudioTest/page.tsx
@@ -10,8 +10,13 @@ export default function RecordingView() {
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const audioChunksRef = useRef<Blob[]>([]);
   const audioElementRef = useRef<HTMLAudioElement | null>(null);
+  const analyserRef = useRef<AnalyserNode | null>(null);
+  const vadTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
+    if (isRecording) {
+      startAudioStream();
+    }
     return () => {
       if (audioContextRef.current) {
         audioContextRef.current.close();
@@ -28,64 +33,66 @@ export default function RecordingView() {
       //AudioContext를 추적하는 useRef에 audioContext를 저장
       audioContextRef.current = audioContext;
 
-      //MediaRecorder를 생성하고, useRef에 저장
-      const mediaRecorder = new MediaRecorder(stream);
-      mediaRecorderRef.current = mediaRecorder;
-      //오디오 녹음이 시작되면 chunks 배열에 데이터를 저장
-      mediaRecorder.ondataavailable = (event) => {
-        audioChunksRef.current.push(event.data);
-      };
-
-      mediaRecorder.onstop = async () => {
-        const audioBlob = new Blob(audioChunksRef.current, {
-          type: 'audio/mp3',
-        });
-        const url = URL.createObjectURL(audioBlob);
-        setAudioUrl(url);
-        const formData = new FormData();
-        formData.append('file', audioBlob);
-        formData.append('model', 'whisper-1');
-        audioChunksRef.current = [];
-
-        try {
-          const response = await fetch(
-            'https://api.openai.com/v1/audio/transcriptions',
-            {
-              method: 'POST',
-              headers: {
-                Authorization: `Bearer sk-G51yx7dlIhhTvyH1wMGgCJbljLYvCNbnoT31NHCwssT3BlbkFJAkhxe6GU7JewkIfGI8TBXagmKDfdcJaJ7h2qWpE1oA`,
-              },
-              body: formData,
-            },
-          );
-          if (response.ok) {
-            const result = await response.json();
-            console.log(result.text);
-            setTranscript(result.text);
-          } else {
-            console.error('Error1:', response.status, response.statusText);
-          }
-        } catch (error) {
-          console.error('Error2:', error);
-        }
-      };
-
       //오디오 소스 설정 : 실시간 음성을 받아오면 createMediaStreamSource로, 파일을 받아오면 createMediaElementSource로
       const audioSource = audioContext.createMediaStreamSource(stream);
       //오디오 소스를 출력하는 노드 생성 : 음성 스트림을 MediaStreamAudioDestinationNode에 저장시킴
       //이 DestinationNode는 WebRTC와 연계되어, 사용자 컴퓨터에 저장되거나 다른 사용자에게 보낼 수 있음
-      const audioDestination = audioContext.createMediaStreamDestination();
+      const audioAnalyser = audioContext.createAnalyser();
+      audioAnalyser.fftSize = 2048;
 
       //오디오 소스와 출력 노드를 연결
-      audioSource.connect(audioDestination);
+      audioSource.connect(audioAnalyser);
 
-      //현재 페이지에 audio태그로 되어있는 컴포넌트가 마운트 되어있다면
-      if (audioElementRef.current) {
-        //해당 컴포넌트에 오디오 소스를 연결
-        audioElementRef.current.srcObject = audioDestination.stream;
-        //재생시키기
-        audioElementRef.current.play();
-      }
+      analyserRef.current = audioAnalyser;
+
+      //MediaRecorder를 생성하고, useRef에 저장
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorderRef.current = mediaRecorder;
+      console.log('시작');
+      //오디오 녹음이 시작되면 chunks 배열에 데이터를 저장
+      mediaRecorder.ondataavailable = async (event) => {
+        if (event.data.size > 0) {
+          audioChunksRef.current.push(event.data);
+        }
+        console.log('222');
+
+        if (audioChunksRef.current.length >= 1) {
+          const audioBlob = new Blob(audioChunksRef.current, {
+            type: 'audio/mp3',
+          });
+          const url = URL.createObjectURL(audioBlob);
+          setAudioUrl(url);
+          const formData = new FormData();
+          formData.append('file', audioBlob);
+          formData.append('model', 'whisper-1');
+
+          try {
+            console.log('보내기직전');
+            const response = await fetch(
+              'https://api.openai.com/v1/audio/transcriptions',
+              {
+                method: 'POST',
+                headers: {
+                  Authorization: `Bearer sk-G51yx7dlIhhTvyH1wMGgCJbljLYvCNbnoT31NHCwssT3BlbkFJAkhxe6GU7JewkIfGI8TBXagmKDfdcJaJ7h2qWpE1oA`,
+                },
+                body: formData,
+              },
+            );
+            if (response.ok) {
+              const result = await response.json();
+              console.log(result.text);
+              setTranscript(result.text);
+              /* 서버로 transcript 보내는 함수 호출 지점 */
+            } else {
+              console.error('Error1:', response.status, response.statusText);
+            }
+          } catch (error) {
+            console.error('Error2:', error);
+          }
+          console.log('3333');
+          audioChunksRef.current = [];
+        }
+      };
 
       //녹음 시작
       mediaRecorder.start();
@@ -112,21 +119,24 @@ export default function RecordingView() {
     setIsRecording(false);
   };
 
+  //   useEffect(() => {
+  //     startAudioStream();
+  //     return () => {
+  //         if (mediaRecorderRef.current) {
+
+  //         }
+  //     }
+  //   })
+
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Audio Playback</h1>
+      <h1>Audio Transcript</h1>
       <button onClick={isRecording ? stopAudioStream : startAudioStream}>
         {isRecording ? 'Stop Recording' : 'Start Recording'}
       </button>
-      {audioUrl ? (
-        <div style={{ marginTop: '20px' }}>
-          <h3>Playback</h3>
-          <audio controls src={audioUrl}></audio>
-          <textarea defaultValue={transcript}></textarea>
-        </div>
-      ) : (
-        <audio ref={audioElementRef} controls style={{ marginTop: '20px' }} />
-      )}
+      <div style={{ marginTop: '20px' }}>
+        <textarea value={transcript} readOnly></textarea>
+      </div>
     </div>
   );
 }
