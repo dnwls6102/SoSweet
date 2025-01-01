5b15ddc80e2271d9326103747c52914088fdb6e1
sookyungchae12 <sookungchae@gmail.com>
2025-01-01 14:53:08 +0900

    db 연결

--- express/src/config/dbConnect.ts
+++ express/src/config/dbConnect.ts
@@ -5,9 +5,10 @@ require('dotenv').config();
 
 const dbConnect = async () => {
     try {
-        const dbURL: string = `mongodb://${process.env.MDB_USER}:${process.env.MDB_PW}@localhost:27017`; // DB 접속 url 
+        const dbURL: string = `${process.env.MONGO_URI}`; // DB 접속 url 
         // 환경 변수에 저장한 DB 연결 코드로 MongoDB 연결
-        await mongoose.connect(dbURL, {dbName: process.env.DB_NAME});
+        await mongoose.connect(dbURL, {dbName: "sosweet"});
+        // await mongoose.connect(dbURL);
         console.log('DB Connected.');
     } catch (err) {
         console.log(err);
