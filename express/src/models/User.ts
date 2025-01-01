c41d7f2274bf11db8f801ad184df72726128c3a1
김수환 <115762875+SooBookE@users.noreply.github.com>
2025-01-02 01:08:25 +0900

    [feat] 도현이형 요청 - id 합친 것 클라이언트로 보내기, 민영이 요청 - 클라이언트에서 영상 서버로 보내기 구현

--- express/src/models/User.ts
+++ express/src/models/User.ts
@@ -10,30 +10,30 @@ export enum State {
 export interface IUser extends Document {
   _id: Types.ObjectId;
   user_id: string;
-    user_password: string;
-    user_nickname: string;
-    user_birth: Date;
-    user_job: string;
-    user_photo: string;
+    // user_password: string;
+    // user_nickname: string;
+    // user_birth: Date;
+    // user_job: string;
+    // user_photo: string;
   user_gender: "남성" | "여성";
-    user_level: "Bronze" | "Silver";
+    // user_level: "Bronze" | "Silver";
   user_state: State;
 }
 
 const UserSchema: Schema = new Schema({
   user_id: { type: String, required: true, unique: true },
-    user_password: { type: String, required: true },
-    user_nickname: { type: String, required: true },
-    user_birth: { type: Date, required: true },
-    user_job: { type: String, required: true },
-    user_photo: { type: String }, // 선택 필드
+    // user_password: { type: String, required: true },
+    // user_nickname: { type: String, required: true },
+    // user_birth: { type: Date, required: true },
+    // user_job: { type: String, required: true },
+    // user_photo: { type: String }, // 선택 필드
   user_gender: { type: String, enum: ["남성", "여성"], required: true },
-    user_level: {
-      type: String,
-      enum: ["Bronze", "Silver"],
-      required: true,
-      default: "Bronze",
-    },
+    // user_level: {
+    //   type: String,
+    //   enum: ["Bronze", "Silver"],
+    //   required: true,
+    //   default: "Bronze",
+    // },
   user_state: {
     type: Number,
     enum: State,
