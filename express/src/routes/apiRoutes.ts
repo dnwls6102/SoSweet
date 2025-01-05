57b4348f0f1143d1c8285860d0ad67fc1c62bfd9
minyeong-lee <lmy1366@gmail.com>
2025-01-06 01:52:30 +0900

    [feat] 백엔드 감정분석 요청, 동작분석 요청 처리 구현

--- express/src/routes/apiRoutes.ts
+++ express/src/routes/apiRoutes.ts
@@ -4,7 +4,7 @@ import { recordDialog, endChat } from "../middlewares/talkWithPerson";
 import { chatMiddleware ,endChatWithAI } from "../middlewares/talkWithAI";
 import { chatAnalysis } from "../middlewares/chatAnalysis";
 import { ttsMiddleware } from "../middlewares/tts";
-
+import { sendFaceInfoToFlask, sendMotionInfoFlask } from "../controllers/flaskController";
 
 
 const api = Router();
@@ -18,6 +18,18 @@ api.post("/ai/dialog/end", endChatWithAI, chatAnalysis);
 api.post("/human/dialog", recordDialog);
 
 api.post("/human/dialog/end", endChat, chatAnalysis);
+
+
+// flask 서버와 연결
+api.post("/human/faceinfo", (req, res) => {
+    console.log("Face info 요청 들어옴:", req.body);
+    sendFaceInfoToFlask(req, res);
+});
+
+api.post("/human/actioninfo", (req, res) => {
+    console.log("Action info 요청 들어옴:", req.body);
+    sendMotionInfoFlask(req, res);
+});
 
 
 export default api;
