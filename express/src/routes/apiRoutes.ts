accc4ef95252ad5773cc7b4c62b4c0749cd0e7a2
Dohyun <168682591+DANDIL1ON@users.noreply.github.com>
2025-01-12 14:44:46 +0900

    프롬프트 생성기 도입

--- express/src/routes/apiRoutes.ts
+++ express/src/routes/apiRoutes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 // import logInRequired from "../middlewares/loginRequired";
 import { recordDialog, endChat } from "../middlewares/talkWithPerson";
-import { initChat,chatMiddleware ,endChatWithAI } from "../middlewares/talkWithAI";
+import {
+  initChat,
+  chatMiddleware,
+  endChatWithAI,
+} from "../middlewares/talkWithAI";
 import { chatAnalysis, getAnalysis } from "../middlewares/chatAnalysis";
-import { initTTS,ttsMiddleware } from "../middlewares/tts";
+import { initTTS, ttsMiddleware } from "../middlewares/tts";
 import { sendFrameInfoToFlask } from "../controllers/flaskController";
-
+import genPrompt from "../middlewares/createPrompt";
 
 const api = Router();
 
-api.post("/ai/dialog/start", initChat, initTTS);
+api.post("/ai/dialog/start", genPrompt, initChat, initTTS);
 
 api.post("/ai/dialog", chatMiddleware, ttsMiddleware);
 
@@ -23,8 +27,8 @@ api.post("/human/dialog/analysis", getAnalysis);
 
 // flask 서버와 연결
 api.post("/human/frameInfo", (req, res) => {
-    // console.log("Frame info 요청 들어옴:", req.body);
-    sendFrameInfoToFlask(req, res);
+  // console.log("Frame info 요청 들어옴:", req.body);
+  sendFrameInfoToFlask(req, res);
 });
 
 export default api;
