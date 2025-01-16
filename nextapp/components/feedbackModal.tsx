124cc271e576bd07d7c9127692142f10290f0800
sookyungchae12 <sookungchae@gmail.com>
2025-01-16 22:05:51 +0900

    [fix] chatai 자동 스크롤 문법 고침, chatbot 메시지 뜰 때만 뜨는 걸로 수정, warningmodal 위치 조정

--- nextapp/components/feedbackModal.tsx
+++ nextapp/components/feedbackModal.tsx
@@ -10,13 +10,6 @@ export default function FeedbackModal({ message }: FeedbackModalProps) {
     <div className={styles.modalContainer}>
       <div className={styles.modalContent}>
         <div className={styles.warningIcon}>
-          <Image 
-            src="/warning.svg" 
-            alt="warningIcon" 
-            width={55} 
-            height={55} 
-            className={styles.warningIcon}
-          />
         </div>
         <p className={styles.message}>{message}</p>
       </div>
