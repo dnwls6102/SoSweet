71e418425147244b18c3f3d733d3e21824b166c5
Ubuntu <ubuntu@ip-172-31-1-93.ap-northeast-2.compute.internal>
2025-01-17 14:48:44 +0000

    [fix] 매칭 중 로그아웃 시 서버 대기열에서 제거하도록 수정

--- nextapp/components/navbarIcon.tsx
+++ nextapp/components/navbarIcon.tsx
@@ -4,11 +4,23 @@ import styles from './navbarIcon.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { setReduxSocket, setRoom } from '../store/socketSlice';
 
 export default function NavbarIcon() {
   const router = useRouter();
+  const socket = useSelector((state: RootState) => state.socket.socket);
+  const dispatch = useDispatch();
 
   const tryLogout = async () => {
+    // 로그아웃 시 서버의 대기열에서 제거하기
+    if (socket) {
+      socket.emit('match-disconnect');
+      socket.disconnect();
+      dispatch(setReduxSocket(null));
+      dispatch(setRoom(null));
+    }
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_SERVER_URL}/users/logout`,
